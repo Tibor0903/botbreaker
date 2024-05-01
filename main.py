50c7bed11cc3f64ab04d189c91ffd697d4f5f765
lazu-o <lazu.publicmail@mail.ru>
2024-05-01 17:03:02 +0300

    Added create_task command Changed some attributes in tasks table (in loadDatabase())

--- main.py
+++ main.py
@@ -43,14 +43,28 @@ def getCurrentTime() -> str:
     return time.strftime("%d/%m/%Y %H:%M:%S UTC", time.gmtime())
 
 
+def createTaskEmbed(id: int, dpt: str, desc: str, finished: bool) -> discord.Embed:
+
+    color_hex_value = "3DF991" if finished else "3D9AF9"
+
+    color = discord.colour.parse_hex_number(color_hex_value)
+    embed = discord.Embed(color=color, title=desc)
+
+    embed.set_author(name=dpt)
+    embed.set_footer(text="ID: "+str(id))
+
+    return embed
+
+
 async def loadDatabase() -> None:
 
     client.db = await asql.connect("main.db")
     cursor = await client.db.cursor()
 
     async with cursor as c:
-        await c.execute(("CREATE TABLE IF NOT EXISTS tasks (id INTEGER, msg_id INTEGER, department_name STRING,"
-                   "task_description STRING, status BOOLEAN, assigned_people JSON, steps JSON);"
+        await c.execute(("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, msg_id INTEGER, "
+                   "department_name STRING, task_description STRING, status BOOLEAN, "
+                   "assigned_people JSON, steps JSON);"
                    ))
         
     await client.db.commit()
@@ -98,6 +112,33 @@ async def embedtest(intr):
     embed.add_field(name="task 1", value="- step 1 (:white_check_mark:) \n- step 2", inline=False)
 
     await intr.response.send_message('', embed=embed)
+
+
+@client.tree.command()
+@app_commands.rename(dpt="department_name", desc="task_description")
+@app_commands.describe(dpt="The department's name", 
+                       desc="The task's description (Animate something, code something, etc.)")
+async def create_task(intr :discord.Interaction, dpt :str, desc :str):
+    """Creates a task"""
+
+    c: asql.Cursor = await client.db.cursor()
+    id: int
+  
+    await c.execute(("INSERT INTO tasks (department_name, task_description, status) VALUES (?, ?, ?)"
+                        "RETURNING id;"), (dpt, desc, False))
+        
+    id = (await c.fetchone())[0]
+
+    await client.db.commit()
+    await intr.response.send_message("Created a task", ephemeral=True)
+
+    embed = createTaskEmbed(id, dpt, desc, False)
+    msg = await intr.channel.send(embed=embed)
+    
+    await c.execute("UPDATE tasks SET msg_id = ? WHERE id = ?;", (msg.id, id))
+
+    await client.db.commit()
+    await c.close()
 
 
 
