d10fbc7a75b0791bbc04aeebace77f18e3ebb26c
lazu-o <lazu.publicmail@mail.ru>
2024-05-01 20:54:39 +0300

    Added list_tasks, assign_people, delete_task (thanks, Tibor) commands Added descriptions to create_task command ID now shows in task embed intents.members is now True

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, time, os, aiosqlite as asql
+import discord, time, os, json, aiosqlite as asql
 from discord import app_commands
 
 from dotenv import load_dotenv
@@ -29,10 +29,11 @@ class ModifiedClient(discord.Client):
 
 
 intents = discord.Intents.default()
-#intents.members = True
+intents.members = True
 
 client = ModifiedClient(intents = intents)
 debug_guild = discord.Object(id = debug_guild_id)
+
 
 
 #-#-// Functions //-#-#
@@ -56,16 +57,29 @@ def createTaskEmbed(id: int, dpt: str, desc: str, finished: bool) -> discord.Emb
     return embed
 
 
+def createJSONOfAssignedPeople(json_str: str | None, user: discord.User) -> str:
+
+    assigned_people = {"user_ids":[]} if not json_str else json.loads(json_str)
+    
+    assigned_people["user_ids"].append(user.id)
+    return json.dumps(assigned_people)
+
+
 async def loadDatabase() -> None:
 
     client.db = await asql.connect("main.db")
     cursor = await client.db.cursor()
 
     async with cursor as c:
-        await c.execute(("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, msg_id INTEGER, "
-                   "department_name STRING, task_description STRING, status BOOLEAN, "
-                   "assigned_people JSON, steps JSON);"
-                   ))
+        await c.execute(("CREATE TABLE IF NOT EXISTS tasks ("
+                    "id INTEGER PRIMARY KEY, "
+                    "msg_id INTEGER, "
+                    "department_name STRING, "
+                    "task_name STRING, "
+                    "status BOOLEAN, "
+                    "assigned_people JSON, "
+                    "steps JSON);"
+                    ))
         
     await client.db.commit()
 
@@ -114,31 +128,90 @@ async def embedtest(intr):
     await intr.response.send_message('', embed=embed)
 
 
-@client.tree.command()
-@app_commands.rename(dpt="department_name", desc="task_description")
-@app_commands.describe(dpt="The department's name", 
-                       desc="The task's description (Animate something, code something, etc.)")
-async def create_task(intr :discord.Interaction, dpt :str, desc :str):
-    """Creates a task"""
+@client.tree.command(description="Creates a task")
+@app_commands.rename(dpt="department_name", name="task_name")
+@app_commands.describe(dpt="the department's name", 
+                       name="task's name (animate something, code something, etc.)")
+async def create_task(intr :discord.Interaction, dpt :str, name :str):
 
     c: asql.Cursor = await client.db.cursor()
     id: int
   
-    await c.execute(("INSERT INTO tasks (department_name, task_description, status) VALUES (?, ?, ?)"
-                        "RETURNING id;"), (dpt, desc, False))
+    await c.execute(("INSERT INTO tasks (department_name, task_name, status) VALUES (?, ?, ?)"
+                        "RETURNING id;"), (dpt, name, False))
         
     id = (await c.fetchone())[0]
 
     await client.db.commit()
     await intr.response.send_message("Created a task", ephemeral=True)
 
-    embed = createTaskEmbed(id, dpt, desc, False)
+    embed = createTaskEmbed(id, dpt, name, False)
     msg = await intr.channel.send(embed=embed)
     
     await c.execute("UPDATE tasks SET msg_id = ? WHERE id = ?;", (msg.id, id))
 
     await client.db.commit()
     await c.close()
+
+
+@client.tree.command(description="Deletes a chosen task")
+@app_commands.rename(id="task_id")
+@app_commands.describe(id="id of the task you want to delete")
+async def delete_task(intr: discord.Interaction, id: int):
+
+    c: asql.Cursor = await client.db.cursor()
+
+    async with c:
+        await c.execute(f"DELETE FROM tasks WHERE id = ?;", [id])
+        await client.db.commit()
+
+    await intr.response.send_message(f"Deleted task under id {id}.")    
+
+
+@client.tree.command()
+async def list_tasks(intr: discord.Interaction):
+    
+    c: asql.Cursor = await client.db.cursor()
+    response = ""
+
+    async with c:
+        await c.execute("SELECT * FROM tasks;")
+        tasks = await c.fetchall()
+
+    for task in tasks:
+
+        id, dpt_name, name = task[0], task[2], task[3]
+        finished = True if task[4] else False
+
+        response += ("```"
+                     f"Task{id}: ID - {id}\n"
+                     f"    ({dpt_name}) {name}\n"
+                     f"    Status: {"Done" if finished else "Not finished"}"
+                     "```\n\n"
+                    )
+    
+    await intr.response.send_message(response)
+
+
+@client.tree.command(description="Assigns chosen user to a task")
+@app_commands.rename(id="task_id")
+@app_commands.describe(id="id of the task you want to assign the user to",
+                       user="the user you want to assign to the task (automatically selects you if skipped)")
+async def assign_people(intr: discord.Interaction, id: int, user: discord.User = None):
+
+    c: asql.Cursor = await client.db.cursor()
+    user = user if user else intr.user
+
+    await c.execute("SELECT assigned_people FROM tasks WHERE id = ?;", [id])
+    stored_assigned_people = (await c.fetchone())[0]
+
+    assigned_people = createJSONOfAssignedPeople(stored_assigned_people, user)
+
+    await c.execute("UPDATE tasks SET assigned_people = ? WHERE id = ?;", [assigned_people, id])
+    await client.db.commit()
+    await c.close()
+    
+    await intr.response.send_message(f"Assigned {user.mention} to Task{id}!", ephemeral=True)
 
 
 
